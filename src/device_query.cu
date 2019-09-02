d06f3b1064f3cab3a30dccdc8b76a3d4c67131e8
Marvin Reza <marvinreza@outlook.com>
2019-09-02 20:43:51 +0200

    less dependencies

--- src/device_query.cu
+++ src/device_query.cu
@@ -1,15 +1,14 @@
 #include "device_query.h"
-#include "logger.h"
 #include "cuda_runtime.h"
 
-#include <assert.h>
+#include <stdio.h>
 
 #define CUDA_CHECK(condition) \
   /* Code block avoids redefinition of cudaError_t error */ \
   do { \
     cudaError_t error = condition; \
     if(error != cudaSuccess) { \
-        LOG_ERROR("%s", cudaGetErrorString(error)); \
+        printf("%s", cudaGetErrorString(error)); \
     } \
   } while (0)
 
@@ -19,30 +18,30 @@ int device_query();
     cudaDeviceProp prop;
 
     if (cudaGetDevice(&device) != cudaSuccess) {
-        LOG_ERROR("No CUDA device present.");
+        printf("No CUDA device present.");
         return 0;
     }
 
     CUDA_CHECK(cudaGetDeviceProperties(&prop, device));
 
-    LOG_INFO("Device id:                     %d", device);
-    LOG_INFO("Major revision number:         %d", prop.major);
-    LOG_INFO("Minor revision number:         %d", prop.minor);
-    LOG_INFO("Name:                          %s", prop.name);
-    LOG_INFO("Total global memory:           %zu", prop.totalGlobalMem);
-    LOG_INFO("Total shared memory per block: %zu", prop.sharedMemPerBlock);
-    LOG_INFO("Total registers per block:     %d", prop.regsPerBlock);
-    LOG_INFO("Warp size:                     %d", prop.warpSize);
-    LOG_INFO("Maximum memory pitch:          %zu", prop.memPitch);
-    LOG_INFO("Maximum threads per block:     %d", prop.maxThreadsPerBlock;
-    LOG_INFO("Maximum dimension of block:    (%d, %d, %d)", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
-    LOG_INFO("Maximum dimension of grid:     (%d, %d, %d)", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]); 
-    LOG_INFO("Clock rate:                    %d", prop.clockRate);
-    LOG_INFO("Total constant memory:         %zu", prop.totalConstMem);
-    LOG_INFO("Texture alignment:             %zu", prop.textureAlignment);
-    LOG_INFO("Concurrent copy and execution: %s", (prop.deviceOverlap ? "Yes" : "No"));
-    LOG_INFO("Number of multiprocessors:     %d", prop.multiProcessorCount);
-    LOG_INFO("Kernel execution timeout:      %s", (prop.kernelExecTimeoutEnabled ? "Yes" : "No"));
+    printf("Device id:                     %d", device);
+    printf("Major revision number:         %d", prop.major);
+    printf("Minor revision number:         %d", prop.minor);
+    printf("Name:                          %s", prop.name);
+    printf("Total global memory:           %zu", prop.totalGlobalMem);
+    printf("Total shared memory per block: %zu", prop.sharedMemPerBlock);
+    printf("Total registers per block:     %d", prop.regsPerBlock);
+    printf("Warp size:                     %d", prop.warpSize);
+    printf("Maximum memory pitch:          %zu", prop.memPitch);
+    printf("Maximum threads per block:     %d", prop.maxThreadsPerBlock;
+    printf("Maximum dimension of block:    (%d, %d, %d)", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
+    printf("Maximum dimension of grid:     (%d, %d, %d)", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]); 
+    printf("Clock rate:                    %d", prop.clockRate);
+    printf("Total constant memory:         %zu", prop.totalConstMem);
+    printf("Texture alignment:             %zu", prop.textureAlignment);
+    printf("Concurrent copy and execution: %s", (prop.deviceOverlap ? "Yes" : "No"));
+    printf("Number of multiprocessors:     %d", prop.multiProcessorCount);
+    printf("Kernel execution timeout:      %s", (prop.kernelExecTimeoutEnabled ? "Yes" : "No"));
 
     return 1;
 }
