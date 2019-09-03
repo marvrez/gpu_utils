39fc6ce058cc3505bd6d347eddbab1e906cea981
Marvin Reza <marvinreza@outlook.com>
2019-09-03 14:11:32 +0200

    prepare for more utilities

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include "device_query.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 int main(int argc, char** argv) 
 {
-    device_query();
+    if(argc < 2) {
+        fprintf(stderr, "usage: %s <function>\n", argv[0]);
+        return 0;
+    }
+    if(strcmp(argv[1], "query") == 0) {
+        device_query();
+    }
+    else {
+        fprintf(stderr, "%s is not a valid option\n", argv[1]);
+    }
+
     return 0;
 }
